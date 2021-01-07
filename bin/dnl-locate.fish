4ab6bd2435fa04cf13aac4d3d35ab57cba73417b
Daniel Correa <dnlcorrea@gmail.com>
2021-01-07 11:36:52 -0300

    Thu Jan  7 11:36:52 -03 2021

--- bin/dnl-locate.fish
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xdg-open "$(rg /home/daniel --files --ignore=node_modules,vendor | rofi -threads 0 -i -p 'file' -dmenu)"
--- bin/tmux.yo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-tmux new-session -c "$(find /home/daniel -not -path '*/\.*' -maxdepth 3 -type d | rofi -dmenu)"
